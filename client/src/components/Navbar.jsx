bb00c095ab5eff03172f9fce57941a890debe334
Alex <ajhollid@gmail.com>
2019-02-21 09:22:48 -0800

    Add register button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,6 +11,7 @@ class Navbar extends Component {
           <div className="col-xs"><div className="nav-item nav-item-spacer" /></div>
           <div className="col-xs-12 col-md-2"><div className="nav-item"><input className="input-search" /></div></div>
           <div className="col-xs-12 col-md-1 nav-login"><div className="nav-item ">Login</div></div>
+          <div className="col-xs-12 col-md-1 nav-login"><div className="nav-item ">Register</div></div>
         </div>
       </div>
     );
