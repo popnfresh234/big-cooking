412b647477fc23c4f35d7dd7780f8102a73a7907
Alex <ajhollid@gmail.com>
2019-02-21 11:20:34 -0800

    Add link colors, fix transition times

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,14 +25,14 @@ class Navbar extends Component {
     return (
       <div className="content">
         <div className="nav row">
-          <div className="col-xs-12 col-md-1"><NavLink exact to="/"><div className="nav-item">Home</div></NavLink></div>
-          <div className="col-xs-12 col-md-1"><NavLink to="/recipes"><div className="nav-item">All Recipes</div></NavLink></div>
-          <div className="col-xs-12 col-md-1"><NavLink to="/myrecipes"><div className="nav-item">My Recipes</div></NavLink></div>
+          <div className="col-xs-12 col-md-1"><NavLink activeClassName="navlink-active" exact to="/" ><div className="nav-item">Home</div></NavLink></div>
+          <div className="col-xs-12 col-md-1"><NavLink activeClassName="navlink-active" to="/recipes"><div className="nav-item">All Recipes</div></NavLink></div>
+          <div className="col-xs-12 col-md-1"><NavLink activeClassName="navlink-active" to="/myrecipes"><div className="nav-item">My Recipes</div></NavLink></div>
           <div className="col-xs"><div className="nav-item nav-item-spacer" /></div>
           <div className="col-xs-12 col-md-2"><div className="nav-item"><input className="input-search" placeholder="Search..." /></div></div>
-          {this.props.isLoggedIn && <div className="col-xs-12 col-md-1 nav-login"><NavLink to="#"onClick={this.handleLogout}><div className="nav-item ">Logout</div></NavLink></div>}
-          {!this.props.isLoggedIn && <div className="col-xs-12 col-md-1 nav-login"><NavLink to="/login"><div className="nav-item ">Login</div></NavLink></div>}
-          {!this.props.isLoggedIn && <div className="col-xs-12 col-md-1 nav-login"><NavLink to="/register"><div className="nav-item ">Register</div></NavLink></div>}
+          {this.props.isLoggedIn && <div className="col-xs-12 col-md-1 nav-login"><NavLink activeClassName="navlink-active" to="#"onClick={this.handleLogout}><div className="nav-item ">Logout</div></NavLink></div>}
+          {!this.props.isLoggedIn && <div className="col-xs-12 col-md-1 nav-login"><NavLink activeClassName="navlink-active" to="/login"><div className="nav-item ">Login</div></NavLink></div>}
+          {!this.props.isLoggedIn && <div className="col-xs-12 col-md-1 nav-login"><NavLink activeClassName="navlink-active" to="/register"><div className="nav-item ">Register</div></NavLink></div>}
         </div>
       </div>
     );
