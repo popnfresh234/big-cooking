9f24f87e2b31f021c9203ff03ce1d9517045a1cb
Alex <ajhollid@gmail.com>
2019-02-27 22:24:41 -0800

    Refacotr modal, add blur to content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Recipes from './components/pages/Recipes.jsx';
 import RecipeDetails from './components/pages/RecipeDetails.jsx';
 import Register from './components/pages/Register.jsx';
 import Login from './components/pages/Login.jsx';
+import DeleteModal from './components/elements/DeleteModal.jsx';
 import NewRecipe from './components/pages/NewRecipe.jsx';
 
 class App extends Component {
@@ -18,9 +19,13 @@ class App extends Component {
       userId: '',
       userName: '',
       loading: false,
+      showModal: false,
+      deleted: false,
     };
     this.handleAuthState = this.handleAuthState.bind( this );
     this.handleLoading = this.handleLoading.bind( this );
+    this.toggleModal = this.toggleModal.bind( this );
+    this.handleDelete = this.handleDelete.bind( this );
   }
 
   componentWillMount() {
@@ -32,6 +37,25 @@ class App extends Component {
       userId,
       userName,
     } );
+  }
+
+  handleDelete( id ) {
+    this.setState( {
+      deleted: true,
+    } );
+    axios.delete( `/api/recipes/${id}` )
+      .then( () => {
+        this.handleLoading();
+        this.setState( {
+          deleted: false,
+        } );
+      } ).catch( ( err ) => {
+        this.handleLoading();
+        this.setState( {
+          deleted: false,
+        } );
+        console.log( err );
+      } );
   }
 
   handleAuthState( isLoggedIn, userId, userName ) {
@@ -49,125 +73,141 @@ class App extends Component {
     }
   }
 
+  toggleModal( id ) {
+    console.log( !this.state.showModal );
+    this.setState( {
+      id,
+      showModal: !this.state.showModal,
+    } );
+    // this.handleDelete( id );
+  }
+
 
   render() {
+    const contentClassName = this.state.showModal ? 'content blurred' : 'content';
     return (
-      <div className="content">
-        <Navbar
-          isLoggedIn={this.state.isLoggedIn}
-          handleAuthState={this.handleAuthState}
-          handleLoading={this.handleLoading}
-        />
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={props => ( <Recipes
-              {...props}
+      <div>
+        <div className={contentClassName}>
+          <Navbar
+            isLoggedIn={this.state.isLoggedIn}
+            handleAuthState={this.handleAuthState}
+            handleLoading={this.handleLoading}
+          />
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={props => ( <Recipes
+                {...props}
+                isLoggedIn={this.state.isLoggedIn}
+                rootPath="random"
+                key="random"
+                cardsPerRow={12 / 3}
+                userId={this.state.userId}
+              /> )}
+            />
+            <Route
+              path="/recipes"
+              exact
+              render={props => ( <Recipes
+                {...props}
+                isLoggedIn={this.state.isLoggedIn}
+                rootPath="recipes"
+                key="recipes"
+                cardsPerRow={12 / 4}
+                handleLoading={this.handleLoading}
+              /> )}
+            />
+            <PrivateRoute
+              path="/myrecipes"
+              exact
               isLoggedIn={this.state.isLoggedIn}
-              rootPath="random"
-              key="random"
-              cardsPerRow={12 / 3}
+              rootPath="myrecipes"
+              key="myrecipes"
               userId={this.state.userId}
-            /> )}
-          />
-          <Route
-            path="/recipes"
-            exact
-            render={props => ( <Recipes
-              {...props}
-              isLoggedIn={this.state.isLoggedIn}
-              rootPath="recipes"
-              key="recipes"
               cardsPerRow={12 / 4}
+              handleAuthState={this.handleAuthState}
               handleLoading={this.handleLoading}
-            /> )}
-          />
-          <PrivateRoute
-            path="/myrecipes"
-            exact
-            isLoggedIn={this.state.isLoggedIn}
-            rootPath="myrecipes"
-            key="myrecipes"
-            userId={this.state.userId}
-            cardsPerRow={12 / 4}
-            handleAuthState={this.handleAuthState}
-            handleLoading={this.handleLoading}
-            component={Recipes}
-          />
-          <PrivateRoute
-            path="/search"
-            exact
-            isLoggedIn={this.state.isLoggedIn}
-            rootPath="search"
-            key="search"
-            userId={this.state.userId}
-            cardsPerRow={4}
-            handleAuthState={this.handleAuthState}
-            handleLoading={this.handleLoading}
-            component={Recipes}
-          />
-          <PrivateRoute
-            path="/recipe-details/:id"
-            exact
-            isLoggedIn={this.state.isLoggedIn}
-            rootPath="recipe-details"
-            userId={this.state.userId}
-            key="recipe-details"
-            handleAuthState={this.handleAuthState}
-            handleLoading={this.handleLoading}
-            component={RecipeDetails}
-          />
-          <PrivateRoute
-            path="/new-recipe"
-            exact
-            isLoggedIn={this.state.isLoggedIn}
-            rootPath="new-recipe"
-            key="new-recipe"
-            userId={this.state.userId}
-            userName={this.state.userName}
-            handleAuthState={this.handleAuthState}
-            handleLoading={this.handleLoading}
-            component={NewRecipe}
-          />
-          <PrivateRoute
-            path="/edit-recipe/:id"
-            exact
-            isLoggedIn={this.state.isLoggedIn}
-            rootPath="new-recipe"
-            key="new-recipe"
-            userId={this.state.userId}
-            userName={this.state.userName}
-            handleAuthState={this.handleAuthState}
-            handleLoading={this.handleLoading}
-            component={NewRecipe}
-          />
-          <Route
-            path="/login"
-            exact
-            render={props =>
-                    ( <Login
-                      {...props}
-                      handleAuthState={this.handleAuthState}
-                      handleLoading={this.handleLoading}
-                      rootPath="signin"
-                      key="signin"
-                    /> )}
-          />
+              component={Recipes}
+            />
+            <PrivateRoute
+              path="/search"
+              exact
+              isLoggedIn={this.state.isLoggedIn}
+              rootPath="search"
+              key="search"
+              userId={this.state.userId}
+              cardsPerRow={4}
+              handleAuthState={this.handleAuthState}
+              handleLoading={this.handleLoading}
+              component={Recipes}
+            />
+            <PrivateRoute
+              path="/recipe-details/:id"
+              exact
+              isLoggedIn={this.state.isLoggedIn}
+              rootPath="recipe-details"
+              userId={this.state.userId}
+              key="recipe-details"
+              handleAuthState={this.handleAuthState}
+              handleLoading={this.handleLoading}
+              toggleModal={this.toggleModal}
+              deleted={this.state.deleted}
+              handleDelete={this.handleDelete}
+              component={RecipeDetails}
+            />
+            <PrivateRoute
+              path="/new-recipe"
+              exact
+              isLoggedIn={this.state.isLoggedIn}
+              rootPath="new-recipe"
+              key="new-recipe"
+              userId={this.state.userId}
+              userName={this.state.userName}
+              handleAuthState={this.handleAuthState}
+              handleLoading={this.handleLoading}
+              component={NewRecipe}
+            />
+            <PrivateRoute
+              path="/edit-recipe/:id"
+              exact
+              isLoggedIn={this.state.isLoggedIn}
+              rootPath="new-recipe"
+              key="new-recipe"
+              userId={this.state.userId}
+              userName={this.state.userName}
+              handleAuthState={this.handleAuthState}
+              handleLoading={this.handleLoading}
+              component={NewRecipe}
+            />
+            <Route
+              path="/login"
+              exact
+              render={props =>
+                ( <Login
+                  {...props}
+                  handleAuthState={this.handleAuthState}
+                  handleLoading={this.handleLoading}
+                  rootPath="signin"
+                  key="signin"
+                /> )}
+            />
 
-          <Route
-            path="/register"
-            exact
-            render={props =>
-                    ( <Register
-                      {...props}
-                      handleAuthState={this.handleAuthState}
-                      handleLoading={this.handleLoading}
-                      rootPath="register"
-                      key="register"
-                    /> )}
-          />
-        </Switch>
+            <Route
+              path="/register"
+              exact
+              render={props =>
+                ( <Register
+                  {...props}
+                  handleAuthState={this.handleAuthState}
+                  handleLoading={this.handleLoading}
+                  rootPath="register"
+                  key="register"
+                /> )}
+            />
+          </Switch>
+        </div>
+        <DeleteModal show={this.state.showModal} toggleModal={this.toggleModal} handleDelete={this.handleDelete} id={this.state.id} />
       </div>
     );
   }
--- client/src/components/elements/DeleteModal.jsx
+++ client/src/components/elements/DeleteModal.jsx
@@ -5,16 +5,30 @@ import Segment from './Segment.jsx';
 class RecipeDetails extends Component {
   constructor( props ) {
     super( props );
-    this.handleDeleteButton = this.handleDeleteButton.bind( this );
     this.handleKeyUp = this.handleKeyUp.bind( this );
     this.handleOutsideClick = this.handleOutsideClick.bind( this );
+    this.handleBackButton = this.handleBackButton.bind( this );
+    this.handleDeleteButton = this.handleDeleteButton.bind( this );
     this.wrapperRef = React.createRef();
+  }
+
+  removeListeners() {
+    window.removeEventListener( 'keyup', this.handleKeyUp, false );
+    document.removeEventListener( 'click', this.handleOutsideClick, false );
+  }
+
+  handleBackButton() {
+    this.props.toggleModal();
+    this.removeListeners();
   }
 
 
   handleDeleteButton() {
-    this.props.onDelete();
+    if ( this.props.id ) {
+      this.props.handleDelete( this.props.id );
+    }
     this.props.toggleModal();
+    this.removeListeners();
   }
 
   handleKeyUp( e ) {
@@ -22,8 +36,7 @@ class RecipeDetails extends Component {
       27: () => {
         e.preventDefault();
         this.props.toggleModal();
-        window.removeEventListener( 'keyup', this.handleKeyUp, false );
-        document.removeEventListener( 'click', this.handleOutsideClick, false );
+        this.removeListeners();
       },
     };
 
@@ -35,8 +48,7 @@ class RecipeDetails extends Component {
   handleOutsideClick( e ) {
     if ( this.wrapperRef.current && !this.wrapperRef.current.contains( e.target ) ) {
       this.props.toggleModal();
-      window.removeEventListener( 'keyup', this.handleKeyUp, false );
-      document.removeEventListener( 'click', this.handleOutsideClick, false );
+      this.removeListeners();
     }
   }
 
@@ -51,7 +63,7 @@ class RecipeDetails extends Component {
         <div className={showStateClassName}>
           <Segment reactRef={this.wrapperRef} className="modal-container">
             <span><h1>Are you sure?</h1></span>
-            <button className="modal-button" onClick={this.props.toggleModal}>BACK</button>
+            <button className="modal-button" onClick={this.handleBackButton}>BACK</button>
             <button className="modal-button delete" onClick={this.handleDeleteButton}>DELETE</button>
           </Segment>
         </div>
