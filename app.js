354a0a64b6f748c875ac08b4034aab2d0148b3fd
Alex <ajhollid@gmail.com>
2019-03-01 08:59:34 -0800

    Add logo to titlebox

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const cookieSession = require( 'cookie-session' );
 const logger = require( 'morgan' );
 const indexRouter = require( './routes/index' );
 const recipesRouter = require( './routes/recipes' );
+require( 'newrelic' );
+
 
 const app = express();
 
--- client/src/components/elements/TitleBox.jsx
+++ client/src/components/elements/TitleBox.jsx
@@ -4,7 +4,10 @@ class TitleBox extends Component {
   render() {
     return (
       <div>
-        <h1 className="title-header">BIG COOKIN' </h1>
+        <div>
+          <img className="logo" src="/assets/img/logo.png" alt="logo" />
+          <h1 className="title-header">BIG COOKIN'</h1>
+        </div>
         <h4 className="title-sub-header">Sharing recipes for a better world</h4>
       </div>
     );
