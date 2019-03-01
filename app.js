e46a7e9fb3d71b7b6dba5c1cbc2fc1643c0f1a5c
Alex <ajhollid@gmail.com>
2019-02-28 17:00:49 -0800

    Add newrelic

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cookieSession = require( 'cookie-session' );
 const logger = require( 'morgan' );
 const indexRouter = require( './routes/index' );
 const recipesRouter = require( './routes/recipes' );
+require( 'newrelic' );
 
 
 const app = express();
