a4bf2af67df7102a9d123041f4d888f9fe1c6da5
Alex <ajhollid@gmail.com>
2019-03-01 17:36:59 -0800

    Add check for edit and delete buttons, add login check for private routes

--- client/src/components/elements/PrivateRoute.jsx
+++ client/src/components/elements/PrivateRoute.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 
 class PrivateRoute extends Component {
   render() {
+    axios.get( '/api/login/check' )
+      .then( ( result ) => {
+        if ( result.status !== 200 ) {
+          this.props.handleAuthState( false, '', '' );
+        }
+      } );
     const { component: Component, isLoggedIn } = this.props;
     return (
       ( isLoggedIn ) ? <Component {...this.props} /> : <Redirect to="/login" /> );
